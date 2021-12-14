e16fcb7a637055443ecb18254fc7982318af2060
Miriam Maestro <miriam.maestro@zeninternet.co.uk>
2021-12-14 09:20:57 +0000

    Add project files.

---
+++ Day7Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7Arrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            string solution = "";
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ')
+                .ToList()
+                .Select(arrTemp => Convert.ToInt32(arrTemp))
+                .ToList();
+
+            int longi = arr.Count-1;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                solution += arr[i] + " ";
+            }
+            for (int a = n; a <= longi; a++)
+            {
+                solution += arr[a] + " ";
+            }
+            Console.Write(solution);
+
+
+            
+        }
+    }
+}
